c2498caef248757f6323c5fd77d8a2274041eee0
AugustoM94 <email@github.com>
2024-01-19 17:38:12 +0100

    fix on Project controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,30 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Project;
+use App\Models\Technology;
 
 class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::paginate(1);
+        $projects = Project::paginate(3);
 
         return response()->json(
             [
-                'success'=>true,
-                'result'=> $projects,
+                'success' => true,
+                'result' => [$projects, technology::all(), category::all()],
         ]
         );
     }
 
-public function show($slug )
-{
-            $project=Project::where('slug', $slug)->with(['technologies', 'category'])->first();
-            return response()->json(
+    public function show($slug)
+    {
+        $project = Project::where('slug', $slug)->with(['technologies', 'category'])->first();
+
+        return response()->json(
             [
-                'success'=>true,
-                'result'=> $project,
+                'success' => true,
+                'result' => $project,
             ]
         );
-}
+    }
 }
